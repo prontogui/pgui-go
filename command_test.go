6668d88b2a5f2fbd12da7acfd822202593cf100c
Andy Joseph <ahjoseph@me.com>
2024-04-24 08:00:40 -0400

    Added tests for setting fields of Command.

--- command_test.go
+++ command_test.go
@@ -12,3 +12,21 @@ func Test_Attach(t *testing.T) {
 	verifyAllFieldsAttached(t, cmd.Reserved, "Label", "Issued", "Status")
 	verifyBsideFieldsAttached(t, &cmd.BSide, cmd.Reserved)
 }
+
+func Test_FieldSetting(t *testing.T) {
+	cmd := &Command{}
+	cmd.SetIssued(true)
+	if !cmd.Issued() {
+		t.Error("Could not set Issued field.")
+	}
+
+	cmd.SetLabel("My label")
+	if cmd.Label() != "My label" {
+		t.Error("Could not set Label field.")
+	}
+
+	cmd.SetStatus(2)
+	if cmd.Status() != 2 {
+		t.Error("Could not set Status field.")
+	}
+}
